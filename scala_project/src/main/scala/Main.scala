566af30a5ed96c89ff855c1d73b7619e466f556c
Carine Zhang <zh.carine@gmail.com>
2018-05-20 01:19:44 +0200

    add Query Option

--- scala_project/src/main/scala/Main.scala
+++ scala_project/src/main/scala/Main.scala
@@ -13,24 +13,58 @@ object Main extends App {
   println("Hello, World!")
   val sca = ScannerTest
   Parser.parseCountries()
-  // Parser.parseAirports()
-  // Parser.parseRunways()
-  val res = Db.query[Country].whereEqual("name", "France").fetchOne()
+  Parser.parseAirports()
+  Parser.parseRunways()
+  //val res = Db.query[Country].whereEqual("name", "France").fetchOne()
+  val res = Db.query[Airport].whereEqual("ident", "00A").fetchOne()
   println(res.get)
 
-  //sca.menu()
+  sca.menu()
 }
 
 object ScannerTest {
   def menu() {
-    println("This is the menu.\nType Query or Reports. Type 'exit' to leave.")
     Iterator.continually(io.StdIn.readLine)
       .takeWhile(_ != "exit")
       .foreach{
-        case "Option" => println("THIS")  /* do this */
+        case "Query" => option()
         case "Reports" => println("THAT")  /* do that */
         case e      => println("Does not recognize this option.")       /* default */
       }
   }
+
+  def option() {
+    println("Query Option: Enter a country name or code")
+    val input = io.StdIn.readLine
+    val nameQuery = Db.query[Country].whereEqual("name", input)
+    if (nameQuery.count() > 0) {
+      fetchAirports(nameQuery.fetchOne().get)
+    }
+    val codeQuery = Db.query[Country].whereEqual("code", input)
+    if (codeQuery.count() > 0){
+      fetchAirports(codeQuery.fetchOne().get)
+    }
+  }
   
+  def fetchAirports(c: Country){
+    val query = Db.query[Airport].whereEqual("country_code", c)
+    val count = query.count()
+    println(s"${c.name}, ${c.continent}: ${count} airports.")
+    if (count > 0) {
+      query.fetch().foreach{
+        x => fetchRunways(x)
+      }
+    }
+  }
+
+  def fetchRunways(a: Airport) {
+    val query = Db.query[Runway].whereEqual("airport_ref", a.id_a)
+    val count = query.count()
+    println(s"${a.name}: $count runways.")
+    if (count > 0) {
+      query.fetch().foreach{
+        x => println(s"- surface: ${x.surface}");
+      }
+    }
+  }
 }
