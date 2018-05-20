6b39ae81dcd986807ad7842ea9235e551369531c
Carine Zhang <zh.carine@gmail.com>
2018-05-20 18:51:07 +0200

    add test for Parser.processString

---
+++ scala_project/src/test/scala/ParserTests.scala
@@ -0,0 +1,9 @@
+import pkg._
+import org.scalatest.FunSuite
+
+class ParserTest extends FunSuite {
+  test("Parser.processString") {
+    assert(Parser.processString("\"string\"") == "string")
+  }
+}
+
